c54c95c9765c355e0abed66f04e008b808cbd2ce
joao gui <jg8467988@gmail.com>
2025-02-19 22:31:23 -0300

    primeira aula js prof aline

---
+++ exercicio 1.js
@@ -0,0 +1,6 @@
+var nome = "joão guilherme";
+console.log (`Olá, meu nome é ${nome}`);
+var idade = 17;
+var altura = 1.80 
+console.log (`olá meu nome é${nome}\ntemho ${idade} anos, ${altura} metros`)
+//entrada de dados exercicio
---
+++ exercicio 2.js
@@ -0,0 +1,19 @@
+var num1 = 5;
+var num2 = 7;
+var soma = (num1 + num2);
+console.log (soma);
+var num3= 5
+var num4= 4
+var subtracao = (num3 - num4);
+console.log (subtracao)
+var multiplicacao = (num3 * num4);
+console.log (multiplicacao)
+var divisao = (num3 / num4);
+console.log (divisao)
+var exponenciacao = (num3 ** num4);
+console.log (exponenciacao)
+var raiz = Math.sqrt (num3)
+console.log (raiz)
+var resto =  (num3 % num4)
+console.log (resto)
+//controladores aritimeticos
